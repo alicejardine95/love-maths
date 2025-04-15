1c93b34c4c4074591bec363b433e86edc6d8fe0b
alicejardine95 <alice.jardine95@yahoo.co.uk>
2025-04-15 10:39:44 +0100

    set up structure of game functions

---
+++ assets/script.js
@@ -0,0 +1,35 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
